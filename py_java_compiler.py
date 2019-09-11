50a340da767bb0d5f92fb28f7497c480a46539f7
Totillity <totillity@gmail.com>
2019-09-10 20:17:36 -0600

    I made the out function into an attribute of the object. See, I know good design.

--- py_java_compiler.py
+++ py_java_compiler.py
@@ -1,11 +1,17 @@
 from __future__ import annotations
 
-import inspect, ast
-
-from typing import List
+import ast
+import inspect
+from typing import List, IO
 
 
 class JavaGenerator(ast.NodeVisitor):
+    def __init__(self, stream: IO):
+        self.stream = stream
+
+    def out(self, text: str):
+        self.stream.write(str(text))
+
     def visit(self, node):
         getattr(self, "visit_"+node.__class__.__name__)(node)
 
@@ -14,13 +20,13 @@ class JavaGenerator(ast.NodeVisitor):
             self.visit(stmt)
 
     def visit_ClassDef(self, node: ast.ClassDef):
-        out("public class ")
-        out(node.name)
-        out(" {\n")
+        self.out("public class ")
+        self.out(node.name)
+        self.out(" {\n")
         for stmt in node.body:
             self.visit(stmt)
-            out("\n")
-        out("}\n")
+            self.out("\n")
+        self.out("}\n")
 
     @staticmethod
     def ann(name):
@@ -31,146 +37,145 @@ class JavaGenerator(ast.NodeVisitor):
         return typ_dict[name]
 
     def visit_Expr(self, node: ast.Expr):
-        out(" " * node.col_offset)
+        self.out(" " * node.col_offset)
         self.visit(node.value)
-        out(";\n")
+        self.out(";\n")
 
     def visit_If(self, node: ast.If):
-        out(" " * node.col_offset)
-        out("if (")
+        self.out(" " * node.col_offset)
+        self.out("if (")
         self.visit(node.test)
-        out(") {\n")
+        self.out(") {\n")
         for stmt in node.body:
             self.visit(stmt)
-        out(" " * node.col_offset)
-        out("}")
+        self.out(" " * node.col_offset)
+        self.out("}")
         if hasattr(node, "orelse"):
-            out(" else {\n")
+            self.out(" else {\n")
             for stmt in node.orelse:
                 self.visit(stmt)
-            out(" " * node.col_offset)
-            out("}")
-        out("\n")
+            self.out(" " * node.col_offset)
+            self.out("}")
+        self.out("\n")
 
     def visit_FunctionDef(self, node: ast.FunctionDef):
 
         if node.name == "__init__":
             instance_vars = [self.ann(arg.annotation.id) + " " + arg.arg for arg in node.args.args if arg.arg != "self"]
             for var in instance_vars:
-                out(" " * node.col_offset)
-                out(var)
-                out(";\n")
+                self.out(" " * node.col_offset)
+                self.out(var)
+                self.out(";\n")
 
-            out("\n")
-            out(" " * node.col_offset)
-            out("public ")
+            self.out("\n")
+            self.out(" " * node.col_offset)
+            self.out("public ")
             cls_name = next(arg.annotation.id for arg in node.args.args if arg.arg == "self")
-            out(cls_name)
-            out("(")
-            out(", ".join(self.ann(arg.annotation.id) + " " + arg.arg for arg in node.args.args if arg.arg != "self"))
-            out(") {\n")
+            self.out(cls_name)
+            self.out("(")
+            self.out(", ".join(self.ann(arg.annotation.id)+" "+arg.arg for arg in node.args.args if arg.arg != "self"))
+            self.out(") {\n")
 
             for stmt in node.body:
                 self.visit(stmt)
-            out(" " * node.col_offset)
-            out("}\n")
+            self.out(" " * node.col_offset)
+            self.out("}\n")
         else:
-            out(" " * node.col_offset)
+            self.out(" " * node.col_offset)
 
-            if any((dec.id == "staticmethod" if isinstance(dec, ast.Name) else False)  for dec in node.decorator_list):
-                out("public static ")
+            if any((dec.id == "staticmethod" if isinstance(dec, ast.Name) else False) for dec in node.decorator_list):
+                self.out("public static ")
             else:
-                out("public ")
+                self.out("public ")
             self.visit(node.returns)
-            # out(self.ann(node.returns.id))
-            out(" ")
-            out(node.name)
+            # self.out(self.ann(node.returns.id))
+            self.out(" ")
+            self.out(node.name)
 
-            out("(")
+            self.out("(")
             sep = ""
             for arg in node.args.args:
                 if arg.arg == "self":
                     continue
                 elif isinstance(arg.annotation, ast.Subscript):
-                    out(sep)
+                    self.out(sep)
                     if isinstance(arg.annotation.value, ast.Name) and arg.annotation.value.id == "List":
                         if isinstance(arg.annotation.slice, ast.Index):
-                            out(self.ann(arg.annotation.slice.value.id))
-                            out("[]")
+                            self.out(self.ann(arg.annotation.slice.value.id))
+                            self.out("[]")
                         else:
                             raise NotImplementedError()
                     else:
                         # print(arg.annotation.value.value)
                         raise NotImplementedError()
                 elif isinstance(arg.annotation, ast.Name):
-                    out(sep)
-                    out(self.ann(arg.annotation.id))
+                    self.out(sep)
+                    self.out(self.ann(arg.annotation.id))
                 else:
                     raise NotImplementedError()
-                out(" ")
-                out(arg.arg)
+                self.out(" ")
+                self.out(arg.arg)
                 sep = ", "
-            # out(", ".join(self.ann(arg.annotation.id) + " " + arg.arg for arg in node.args.args if arg.arg != "self"))
-            out(") {\n")
+            self.out(") {\n")
 
             for stmt in node.body:
                 self.visit(stmt)
 
             # print(node.col_offset)
-            out(" " * node.col_offset)
-            out("}\n")
+            self.out(" " * node.col_offset)
+            self.out("}\n")
 
     def visit_BinOp(self, node: ast.BinOp):
         if isinstance(node.op, ast.Mult):
-            out("(")
+            self.out("(")
             self.visit(node.left)
-            out(" * ")
+            self.out(" * ")
             self.visit(node.right)
-            out(")")
+            self.out(")")
         elif isinstance(node.op, ast.Pow):
-            out("Math.pow(")
+            self.out("Math.pow(")
             self.visit(node.left)
-            out(", ")
+            self.out(", ")
             self.visit(node.right)
-            out(")")
+            self.out(")")
         elif isinstance(node.op, ast.Add):
-            out("(")
+            self.out("(")
             self.visit(node.left)
-            out(" + ")
+            self.out(" + ")
             self.visit(node.right)
-            out(")")
+            self.out(")")
         elif isinstance(node.op, ast.Sub):
-            out("(")
+            self.out("(")
             self.visit(node.left)
-            out(" - ")
+            self.out(" - ")
             self.visit(node.right)
-            out(")")
+            self.out(")")
         else:
             raise NotImplementedError(node.op)
 
     def visit_Compare(self, node: ast.Compare):
         if isinstance(node.ops[0], ast.Gt):
-            out("(")
+            self.out("(")
             self.visit(node.left)
-            out(" > ")
+            self.out(" > ")
             self.visit(node.comparators[0])
-            out(")")
+            self.out(")")
         elif isinstance(node.ops[0], ast.Lt):
-            out("(")
+            self.out("(")
             self.visit(node.left)
-            out(" < ")
+            self.out(" < ")
             self.visit(node.comparators[0])
-            out(")")
+            self.out(")")
         else:
             raise NotImplementedError(node.op)
 
     def visit_NameConstant(self, node: ast.NameConstant):
         if node.value is True:
-            out("true")
+            self.out("true")
         elif node.value is False:
-            out("false")
+            self.out("false")
         elif node.value is None:
-            out("void")
+            self.out("void")
         else:
             raise NotImplementedError()
 
@@ -178,104 +183,98 @@ class JavaGenerator(ast.NodeVisitor):
         if isinstance(node.ctx, ast.Load):
             name = node.id
             if name == "int":
-                out("Integer")
+                self.out("Integer")
             else:
-                out(node.id)
+                self.out(node.id)
         else:
             raise NotImplementedError()
 
     def visit_Attribute(self, node: ast.Attribute):
         if isinstance(node.ctx, ast.Load):
             if isinstance(node.value, ast.Name) and node.value.id == "self":
-                out("this.")
+                self.out("this.")
             else:
-                out(node.value)
-                out(".")
-            out(node.attr)
+                self.out(node.value)
+                self.out(".")
+            self.out(node.attr)
         else:
             raise NotImplementedError()
 
     def visit_Return(self, node: ast.Return):
-        out(" "*node.col_offset)
-        out("return ")
+        self.out(" "*node.col_offset)
+        self.out("return ")
         self.visit(node.value)
-        out(";\n")
+        self.out(";\n")
 
     def visit_Assign(self, node: ast.Assign):
-        out(" " * node.col_offset)
+        self.out(" " * node.col_offset)
 
         target = node.targets[0]
 
         if isinstance(target, ast.Name):
-            out(target.id)
+            self.out(target.id)
         elif isinstance(target, ast.Attribute):
             if isinstance(target.value, ast.Name) and target.value.id == "self":
-                out("this.")
+                self.out("this.")
             else:
                 # self.visit(target.value)
-                out(target.value)
-                out(".")
-            out(target.attr)
+                self.out(target.value)
+                self.out(".")
+            self.out(target.attr)
         else:
             raise NotImplementedError()
 
-        out(" = ")
+        self.out(" = ")
 
         self.visit(node.value)
-        out(";\n")
+        self.out(";\n")
 
     def visit_AnnAssign(self, node: ast.AnnAssign):
-        out(" " * node.col_offset)
+        self.out(" " * node.col_offset)
 
         if isinstance(node.target, ast.Name):
-            out(self.ann(node.annotation.id) + " " + node.target.id)
+            self.out(self.ann(node.annotation.id) + " " + node.target.id)
         elif isinstance(node.target, ast.Attribute):
-            out(self.ann(node.annotation.id))
-            out(" ")
+            self.out(self.ann(node.annotation.id))
+            self.out(" ")
             if isinstance(node.target.value, ast.Name) and node.target.value.id == "self":
                 pass
             else:
-                out(node.target.value)
-                out(".")
-            out(node.target.attr)
+                self.out(node.target.value)
+                self.out(".")
+            self.out(node.target.attr)
         else:
             raise NotImplementedError()
 
-        out(" = ")
+        self.out(" = ")
 
         self.visit(node.value)
-        out(";\n")
+        self.out(";\n")
 
     def visit_Call(self, node: ast.Call):
         if isinstance(node.func, ast.Name) and node.func.id == "int":
-            out("(int) (")
+            self.out("(int) (")
             self.visit(node.args[0])
-            out(")")
+            self.out(")")
         else:
             self.visit(node.func)
-            out("(")
+            self.out("(")
 
             sep = ""
             for arg in node.args:
-                out(sep)
+                self.out(sep)
                 self.visit(arg)
                 sep = ", "
-            out(")")
+            self.out(")")
 
     def visit_Num(self, node: ast.Num):
-        out(node.n)
-
-
-def out(arg):
-    with open("out.java", "a") as file:
-        file.write(str(arg))
+        self.out(node.n)
 
 
 def generate_java(func):
-    with open("out.java", "w"):
-        pass
     code = ast.parse(inspect.getsource(func))
-    JavaGenerator().visit(code)
+    with open("out.java", "w") as file:
+        JavaGenerator(file).visit(code)
 
 
 class Main:
@@ -292,5 +291,6 @@ class Main:
             return n
         else:
             return self.fibo(n-1) + self.fibo(n-2)
+
 
 generate_java(Main)
